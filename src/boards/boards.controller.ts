74ed432dbe3fe8a86192101d5fcc5a9b65d46d35
hyerim Kim <hyerim980513@gmail.com>
2022-06-11 19:57:33 +0900

    feat: CRUD 기능 만들기, 파이프 사용해서 유효성 검사하기

---
+++ src/boards/boards.controller.ts
@@ -0,0 +1,41 @@
+import { Body, Controller, Delete, Get, Param, Patch, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Board, BoardStatus } from './board.model';
+import { BoardsService } from './boards.service';
+import { CreateBoardDto } from './dto/create-board.dto';
+import { BoardStatusValidationPipe } from './pipes/board-status-validation.pipe';
+
+@Controller('boards')
+export class BoardsController {
+    constructor(private boardService: BoardsService){}
+
+    @Get('/')
+    getAllBoard(): Board[]{
+        return this.boardService.getAllBoards();
+    }
+
+    @Post()
+    @UsePipes(ValidationPipe)
+    createBoard(
+        @Body() CreateBoardDto: CreateBoardDto
+        ): Board {
+            return this.boardService.createBoard(CreateBoardDto);
+    }
+ 
+    @Get('/:id')
+    getBoardById(@Param('id') id: string): Board{
+        return this.boardService.getBoardById(id);
+    }
+
+    @Delete('/:id')
+    deleteBoard(@Param('id') id: string): void {
+        this.boardService.deleteBoard(id);
+    }
+
+    @Patch('/:id/status')
+    updateBoardStatus(
+        @Param('id') id: string, 
+        @Body('status', BoardStatusValidationPipe) status:BoardStatus
+    ) {
+        return this.boardService.updateBoardStatus(id,status)
+    }
+}
